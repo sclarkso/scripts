45b6a097f9e734e74e69f8a54aa2e12346602c92
Steven Clarkson <sclarkso@redhat.com>
2017-06-29 10:54:44 +1000

    fixed logging

--- standup.sh
+++ standup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-export SLACK_TOKEN=""
+oldIFS="$IFS"
+export SLACK_TOKEN="
 /usr/local/bin/gshuf -e Alan Takayuki Craig Steven -o standup.out
-echo "-----------" >> standup.log
-date +%Y-%m-%d >> standup.log
-cat standup.out >> standup.log
+IFS=$'\n' arr=($(<standup.out))
+arr=($(date +%Y-%m-%d) "${arr[@]}")
+echo ${arr[*]} >> standup.log
 cat standup.out | /Library/Frameworks/Python.framework/Versions/2.7/bin/slacker -c rhmap-apac -n SKYNET
+IFS=$oldIFS
