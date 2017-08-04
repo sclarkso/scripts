53f50aac2b5e314eb22eda058f6cc3ae5866fbdc
Steven Clarkson <sclarkso@redhat.com>
2017-08-04 13:03:17 +1000

    Update standup.sh

--- standup.sh
+++ standup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 oldIFS="$IFS"
-export SLACK_TOKEN="xoxp-3275597612-78536592288-159846653248-41246486fe463cad40e556485bc7fbd9"
-/usr/local/bin/gshuf -e Alan Takayuki Craig Steven -o standup.out
+export SLACK_TOKEN="XXXXXXXXXXXXXX"
+/usr/local/bin/gshuf -e A T C S-o standup.out
 IFS=$'\n' arr=($(<standup.out))
 arr=($(date +%Y-%m-%d) "${arr[@]}")
 echo ${arr[*]} >> standup.log
-cat standup.out | /Library/Frameworks/Python.framework/Versions/2.7/bin/slacker -c rhmap-apac -n STANDUP_BOT
-echo "https://bluejeans.com/271411487/" | /Library/Frameworks/Python.framework/Versions/2.7/bin/slacker -c rhmap-apac -n STANDUP_BOT
+cat standup.out | /Library/Frameworks/Python.framework/Versions/2.7/bin/slacker -c CHANNEL -n STANDUP_BOT
+echo "https://bluejeans.com/XXXXXXXXX" | /Library/Frameworks/Python.framework/Versions/2.7/bin/slacker -c CHANNEL -n STANDUP_BOT
 IFS=$oldIFS
